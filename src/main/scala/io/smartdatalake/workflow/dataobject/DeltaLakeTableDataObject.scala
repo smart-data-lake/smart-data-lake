a60c314b7c5c672aaffb38e7cfeec1cbfd6c70ed
Nikolaus Thiel <klt@fsfe.org>
2020-03-25 16:13:44 +0100

    DeltaTable Object implemented

---
+++ src/main/scala/io/smartdatalake/workflow/dataobject/DeltaLakeTableDataObject.scala
@@ -0,0 +1,150 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2020 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.smartdatalake.workflow.dataobject
+
+import io.smartdatalake.config.SdlConfigObject.{ConnectionId, DataObjectId}
+import io.smartdatalake.config.{ConfigurationException, FromConfigFactory, InstanceRegistry}
+import io.smartdatalake.definitions.DateColumnType
+import io.smartdatalake.definitions.DateColumnType.DateColumnType
+import io.smartdatalake.util.hdfs.{HdfsUtil, PartitionValues}
+import io.smartdatalake.util.hive.HiveUtil
+import io.smartdatalake.util.misc.{AclDef, AclUtil}
+import io.smartdatalake.workflow.connection.HiveTableConnection
+import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
+
+import scala.collection.JavaConverters._
+
+case class DeltaLakeTableDataObject(override val id: DataObjectId,
+                                    path: String,
+                                    override val partitions: Seq[String] = Seq(),
+                                    analyzeTableAfterWrite: Boolean = false,
+                                    dateColumnType: DateColumnType = DateColumnType.Date,
+                                    override val schemaMin: Option[StructType] = None,
+                                    override var table: Table,
+                                    numInitialHdfsPartitions: Int = 16,
+                                    saveMode: SaveMode = SaveMode.Overwrite,
+                                    acl: Option[AclDef] = None,
+                                    connectionId: Option[ConnectionId] = None,
+                                    override val metadata: Option[DataObjectMetadata] = None)
+                                   (@transient implicit val instanceRegistry: InstanceRegistry)
+  extends TransactionalSparkTableDataObject with CanHandlePartitions {
+
+  /**
+   * Connection defines db, path prefix (scheme, authority, base path) and acl's in central location
+   */
+  private val connection = connectionId.map(c => getConnection[HiveTableConnection](c))
+
+  // prepare final path and table
+  @transient private[workflow] lazy val hadoopPath = HdfsUtil.prefixHadoopPath(path, connection.map(_.pathPrefix))
+  @transient private var filesystemHolder: FileSystem = _
+  def filesystem(implicit session: SparkSession): FileSystem = {
+    if (filesystemHolder == null) {
+      filesystemHolder = HdfsUtil.getHadoopFsFromSpark(hadoopPath)
+    }
+    filesystemHolder
+  }
+  table = table.overrideDb(connection.map(_.db))
+  if (table.db.isEmpty) {
+    throw ConfigurationException(s"($id) db is not defined in table and connection for dataObject.")
+  }
+
+  override def prepare(implicit session: SparkSession): Unit = {
+    require(isDbExisting, s"($id) DB ${table.db.get} doesn't exist (needs to be created manually).")
+  }
+
+  override def getDataFrame(partitionValues: Seq[PartitionValues] = Seq())(implicit session: SparkSession): DataFrame = {
+    val df = session.read.format("delta").load(hadoopPath.toString)
+    validateSchemaMin(df)
+    df
+  }
+
+  override def init(df: DataFrame, partitionValues: Seq[PartitionValues])(implicit session: SparkSession): Unit = {
+    // create tables if possible
+    prepare(session)
+    if (!isTableExisting) {
+      logger.info(s"($id) Creating table ${table.fullName}.")
+      writeDataFrame(df, createTableOnly = true, partitionValues)
+    }
+  }
+
+  override def writeDataFrame(df: DataFrame, partitionValues: Seq[PartitionValues])
+                             (implicit session: SparkSession): Unit = {
+    validateSchemaMin(df)
+    writeDataFrame(df, createTableOnly=false, partitionValues)
+  }
+
+  /**
+   * Writes DataFrame to HDFS/Parquet and creates Hive table.
+   * DataFrames are repartitioned in order not to write too many small files
+   * or only a few HDFS files that are too large.
+   */
+  def writeDataFrame(df: DataFrame, createTableOnly: Boolean, partitionValues: Seq[PartitionValues])
+                    (implicit session: SparkSession): Unit = {
+    val dfPrepared = if (createTableOnly) {
+      // create empty df with existing df's schema
+      session.createDataFrame(List.empty[Row].asJava, df.schema)
+    } else {
+      // pass DataFrame straight through if numInitialHdfsPartitions == -1, in this case the file size in the responsibility of the framework and must be controlled in custom transformations
+      if(numInitialHdfsPartitions == -1) df
+      // estimate number of partitions from existing data, otherwise use numInitialHdfsPartitions
+      else if (isTableExisting) {
+        val currentHdfsPath = HdfsUtil.prefixHadoopPath(HiveUtil.existingTickTockLocation(table.db.get, session, table.name), None)
+        HdfsUtil.repartitionForHdfsFileSize(df, currentHdfsPath.toString)
+      } else df.repartition(numInitialHdfsPartitions)
+    }
+
+    // write table and fix acls
+    if (partitions.isEmpty) dfPrepared.write.format("delta").save(hadoopPath.toString) else {
+      dfPrepared.write.format("delta").partitionBy(partitions:_*).save(hadoopPath.toString)
+    }
+    if (acl.isDefined) {
+      AclUtil.addACLs(acl.get, hadoopPath)(filesystem)
+    }
+    if (analyzeTableAfterWrite && !createTableOnly) {
+      logger.info(s"($id) Analyze table ${table.fullName}.")
+      HiveUtil.analyze(session, table.db.get, table.name, partitions, partitionValues)
+    }
+  }
+
+  override def isDbExisting(implicit session: SparkSession): Boolean = {
+    session.catalog.databaseExists(table.db.get)
+  }
+
+  override def isTableExisting(implicit session: SparkSession): Boolean = {
+    session.catalog.tableExists(table.db.get, table.name)
+  }
+
+  /**
+   * list hive table partitions
+   */
+  override def listPartitions(implicit session: SparkSession): Seq[PartitionValues] = {
+    if(isTableExisting) HiveUtil.listPartitions(table, partitions)
+    else Seq()
+  }
+
+  /**
+   * @inheritdoc
+   */
+  override def factory: FromConfigFactory[DataObject] = TickTockHiveTableDataObject
+}
+
+
+
---
+++ src/test/scala/io/smartdatalake/workflow/action/CustomDfToDeltaTableTest.scala
@@ -0,0 +1,96 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2020 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.smartdatalake.workflow.action
+
+import java.io.File
+import java.time.LocalDateTime
+
+import com.holdenkarau.spark.testing.Utils
+import io.smartdatalake.config.InstanceRegistry
+import io.smartdatalake.testutils.TestUtil
+import io.smartdatalake.testutils.TestUtil._
+import io.smartdatalake.util.misc.DataFrameUtil.DfSDL
+import io.smartdatalake.workflow.action.customlogic.CustomDfCreatorConfig
+import io.smartdatalake.workflow.dataobject.{CustomDfDataObject, DeltaLakeTableDataObject, Table}
+import io.smartdatalake.workflow.{ActionPipelineContext, SparkSubFeed}
+import org.apache.spark.sql.SparkSession
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class CustomDfToDeltaTableTest extends FunSuite with BeforeAndAfter {
+
+  protected implicit val session: SparkSession = TestUtil.sessionHiveCatalog
+
+  val tempDir: File = Utils.createTempDir()
+  val tempPath: String = tempDir.toPath.toAbsolutePath.toString
+
+  implicit val instanceRegistry: InstanceRegistry = new InstanceRegistry
+
+  before { instanceRegistry.clear() }
+
+  test("CustomDf2DeltaTable") {
+
+    // setup DataObjects
+    val feed = "customDf2Delta"
+    val sourceDO = CustomDfDataObject(id="source",creator = CustomDfCreatorConfig(className = Some("io.smartdatalake.config.TestCustomDfCreator")))
+    val targetTable = Table(db = Some("default"), name = "custom_df_copy", query = None, primaryKey = Some(Seq("line")))
+    val targetTablePath = tempPath+s"/${targetTable.fullName}"
+    val targetDO = DeltaLakeTableDataObject(id="target", path=targetTablePath, table=targetTable, numInitialHdfsPartitions=1)
+    instanceRegistry.register(sourceDO)
+    instanceRegistry.register(targetDO)
+
+    // prepare & start load
+    val startTime = LocalDateTime.now()
+    implicit val context: ActionPipelineContext = ActionPipelineContext(feed, "test", instanceRegistry, Some(startTime))
+    val testAction = CopyAction(id = s"${feed}Action", inputId = sourceDO.id, outputId = targetDO.id)
+    val srcSubFeed = SparkSubFeed(None, "source", partitionValues = Seq())
+    testAction.exec(Seq(srcSubFeed))
+
+    val expected = sourceDO.getDataFrame()
+    val actual = targetDO.getDataFrame()
+    val resultat: Boolean = expected.isEqual(actual)
+    if (!resultat) printFailedTestResult("Df2HiveTable",Seq())(actual)(expected)
+    assert(resultat)
+  }
+
+  test("CustomDf2DeltaTable_partitioned") {
+
+    // setup DataObjects
+    val feed = "customDf2Delta_partitioned"
+    val sourceDO = CustomDfDataObject(id="source",creator = CustomDfCreatorConfig(className = Some("io.smartdatalake.config.TestCustomDfCreator")))
+    val targetTable = Table(db = Some("default"), name = "custom_df_copy_partitioned", query = None, primaryKey = Some(Seq("line")))
+    val targetTablePath = tempPath+s"/${targetTable.fullName}"
+    val targetDO = DeltaLakeTableDataObject(id="target", partitions=Seq("num"), path=targetTablePath, table=targetTable, numInitialHdfsPartitions=1)
+    instanceRegistry.register(sourceDO)
+    instanceRegistry.register(targetDO)
+
+    // prepare & start load
+    val startTime = LocalDateTime.now()
+    implicit val context: ActionPipelineContext = ActionPipelineContext(feed, "test", instanceRegistry, Some(startTime))
+    val testAction = CopyAction(id = s"${feed}Action", inputId = sourceDO.id, outputId = targetDO.id)
+    val srcSubFeed = SparkSubFeed(None, "source", partitionValues = Seq())
+    testAction.exec(Seq(srcSubFeed))
+
+    val expected = sourceDO.getDataFrame()
+    val actual = targetDO.getDataFrame()
+    val resultat: Boolean = expected.isEqual(actual)
+    if (!resultat) printFailedTestResult("Df2HiveTable",Seq())(actual)(expected)
+    assert(resultat)
+  }
+
+}
