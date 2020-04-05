6325278a8cc64c839e84643f3a2764cbecd2c2ec
Zacharias Kull <zacharias.kull@elca.ch>
2020-04-05 14:12:38 +0200

    fix/improve text for exception log messages

--- src/main/scala/io/smartdatalake/workflow/ActionDAG.scala
+++ src/main/scala/io/smartdatalake/workflow/ActionDAG.scala
@@ -49,7 +49,7 @@ private[smartdatalake] class ActionEventListener(operation: String) extends DAGE
   }
   override def onNodeFailure(exception: Throwable)(node: Action): Unit = {
     node.addRuntimeEvent(operation, RuntimeEventState.FAILED, s"${exception.getClass.getSimpleName}: ${exception.getMessage}")
-    logger.error(s"${node.toStringShort}: $operation failed with")
+    logger.error(s"${node.toStringShort}: $operation failed with ${exception.getClass.getSimpleName}: ${exception.getMessage}")
   }
   override def onNodeSkipped(exception: Throwable)(node: Action): Unit = {
     node.addRuntimeEvent(operation, RuntimeEventState.SKIPPED, s"${exception.getClass.getSimpleName}: ${exception.getMessage}")
@@ -80,8 +80,8 @@ private[smartdatalake] case class ActionDAGRun(dag: DAG[Action], runId: String, 
       }
       // log all exceptions
       dagExceptions.foreach {
-        case ex if (ex.severity <= ExceptionSeverity.CANCELLED) => logger.error(s"${ex.getClass.getSimpleName}: ${ex.getMessage}")
-        case ex => logger.warn(s"${ex.getClass.getSimpleName}: ${ex.getMessage}")
+        case ex if (ex.severity <= ExceptionSeverity.CANCELLED) => logger.error(s"$op: ${ex.getClass.getSimpleName}: ${ex.getMessage}")
+        case ex => logger.warn(s"$op: ${ex.getClass.getSimpleName}: ${ex.getMessage}")
       }
       // log dag on error
       if (dagExceptions.nonEmpty) ActionDAGRun.logDag(s"$op failed for dag $runId", dag)
