2046fa864f02ac36fdaf97c4edd177ab1add8fed
zkull <zach.kull@gmail.com>
2020-07-02 21:59:24 +0200

    cleanup unused function

--- src/main/scala/io/smartdatalake/workflow/dataobject/CanCreateDataFrame.scala
+++ src/main/scala/io/smartdatalake/workflow/dataobject/CanCreateDataFrame.scala
@@ -26,17 +26,10 @@ private[smartdatalake] trait CanCreateDataFrame {
 
   def getDataFrame(partitionValues: Seq[PartitionValues] = Seq())(implicit session: SparkSession) : DataFrame
 
-  // Default implementation just filters the input dataframe in spark
-  // Concrete implementations should implement pushdown logic for their
-  // respective data sources.
-  def getDeltaDataFrame(filterExpr: Column)(implicit session: SparkSession) : DataFrame = {
-    getDataFrame(Seq()).filter(filterExpr)
-  }
-
   /**
    * Creates the read schema based on a given write schema.
    * Normally this is the same, but some DataObjects can remove & add columns on read (e.g. KafkaTopicDataObject, SparkFileDataObject)
-   * In this cases we have to break the lineage und create a dummy DataFrame in init phase.
+   * In this cases we have to break the DataFrame lineage und create a dummy DataFrame in init phase.
    */
   def createReadSchema(writeSchema: StructType)(implicit session: SparkSession): StructType = writeSchema
 
