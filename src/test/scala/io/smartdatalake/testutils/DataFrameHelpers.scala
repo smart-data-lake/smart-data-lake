b55eabad59a499617c456295bc12d7bf31d9cef6
zzeekk <zach.kull@gmail.com>
2020-11-19 00:04:33 +0100

    revert some merged changes because branch feature/bettertesthelpers was still on the old project layout

--- src/test/scala/io/smartdatalake/testutils/DataFrameHelpers.scala
+++
@@ -1,7 +0,0 @@
-package org.apache.spark.sql
-
-object DataFrameHelpers {
-  def showString[T](df: Dataset[T], _numRows: Int, truncate: Int = 20, vertical: Boolean = false): String = {
-    df.showString(_numRows, truncate, vertical)
-  }
-}
