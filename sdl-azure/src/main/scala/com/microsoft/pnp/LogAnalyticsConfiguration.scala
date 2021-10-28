2977f498fd37c40ad8da5f08c88fe2fa7e94938f
zzeekk <zach.kull@gmail.com>
2021-10-28 08:24:35 +0200

    cleanup, fix pom

---
+++ sdl-azure/src/main/scala/com/microsoft/pnp/LogAnalyticsConfiguration.scala
@@ -0,0 +1,58 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2021 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.microsoft.pnp
+
+import org.apache.spark.internal.Logging
+
+trait LogAnalyticsConfiguration extends Logging {
+  protected def getWorkspaceId: Option[String]
+
+  protected def getSecret: Option[String]
+
+  protected def getLogType: String
+
+  protected def getTimestampFieldName: Option[String]
+
+  val workspaceId: String = {
+    val value = getWorkspaceId
+    require(value.isDefined, "A Log Analytics Workspace ID is required")
+    logInfo(s"Setting workspaceId to ${value.get}")
+    value.get
+
+  }
+
+  val secret: String = {
+    val value = getSecret
+    require(value.isDefined, "A Log Analytics Workspace Key is required")
+    value.get
+  }
+
+  val logType: String = {
+    val value = getLogType
+    logInfo(s"Setting logType to $value")
+    value
+  }
+
+  val timestampFieldName: String = {
+    val value = getTimestampFieldName
+    logInfo(s"Setting timestampNameField to $value")
+    value.orNull
+  }
+}
--- sdl-azure/src/main/scala/org/apache/spark/metrics/sink/loganalytics/LogAnalyticsSinkConfiguration.scala
+++ sdl-azure/src/main/scala/org/apache/spark/metrics/sink/loganalytics/LogAnalyticsSinkConfiguration.scala
@@ -1,10 +1,28 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2021 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.apache.spark.metrics.sink.loganalytics
 
-import com.microsoft.pnp.LogAnalyticsEnvironment
+import com.microsoft.pnp.{LogAnalyticsConfiguration, LogAnalyticsEnvironment}
 
 import java.util.Properties
 import java.util.concurrent.TimeUnit
-import org.apache.spark.com.microsoft.pnp.LogAnalyticsConfiguration
 
 private[spark] object LogAnalyticsSinkConfiguration {
   private[spark] val LOGANALYTICS_KEY_WORKSPACEID = "workspaceId"
