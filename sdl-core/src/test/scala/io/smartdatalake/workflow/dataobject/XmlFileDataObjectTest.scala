c1de4a85344a82b99899f91199043aa2cfc1f1db
zzeekk <zach.kull@gmail.com>
2021-11-05 13:05:10 +0100

    implement reading partitioned xml-data, remove unneeded utils

--- sdl-core/src/test/scala/io/smartdatalake/workflow/dataobject/XmlFileDataObjectTest.scala
+++ sdl-core/src/test/scala/io/smartdatalake/workflow/dataobject/XmlFileDataObjectTest.scala
@@ -18,15 +18,53 @@
  */
 package io.smartdatalake.workflow.dataobject
 
+import io.smartdatalake.definitions.SDLSaveMode
 import io.smartdatalake.testutils.DataObjectTestSuite
+import io.smartdatalake.util.hdfs.PartitionValues
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
 
+import java.nio.file.Files
+import io.smartdatalake.util.misc.DataFrameUtil.DfSDL
+import org.apache.hadoop.fs.Path
+
+
 class XmlFileDataObjectTest extends DataObjectTestSuite with SparkFileDataObjectSchemaBehavior {
+
+  import session.implicits._
 
   testsFor(readNonExistingSources(createDataObject, ".xml"))
   testsFor(readEmptySources(createDataObject, ".xml"))
   testsFor(validateSchemaMinOnWrite(createDataObjectWithSchemaMin, ".xml"))
+
+  // note that spark-xml doesn't support reading and writing partitioned xml data.
+  // SDL implements custom logic to read partitioned xml data
+  test("XML files partitioned") {
+    val tempDir = Files.createTempDirectory("xml")
+
+    val data1 = Seq(("A", "1", "-"), ("B", "2", null))
+    val df1 = data1.toDF("h1", "h2", "h3")
+    val pv1 = Seq(PartitionValues(Map("h1"->"A")), PartitionValues(Map("h1"->"B")))
+
+    // Partitions have to be written manually as spark-xml doesn't support writing partitions
+    val dataObj = XmlFileDataObject(id = "test1", path = escapedFilePath(tempDir.toFile.getPath), schema = Some(df1.schema), filenameColumn = Some("_filename"))
+    dataObj.writeDataFrameToPath(df1.where($"h1"==="A"), new Path(dataObj.hadoopPath, "h1=A"), SDLSaveMode.Overwrite)
+    dataObj.writeDataFrameToPath(df1.where($"h1"==="B"), new Path(dataObj.hadoopPath, "h1=B"), SDLSaveMode.Overwrite)
+
+    val dataObjPartitioned = dataObj.copy(partitions = Seq("h1"))
+
+    // read with list of partition values
+    val dfResult1 = dataObjPartitioned.getDataFrame(pv1).cache
+    assert(dfResult1.columns.toSet == Set("h1", "h2", "h3", "_filename"))
+    assert(dfResult1.drop("_filename").isEqual(df1))
+    assert(dfResult1.where($"_filename".isNull).isEmpty)
+
+    // read all
+    val dfResult2 = dataObjPartitioned.getDataFrame().cache
+    assert(dfResult2.columns.toSet == Set("h1", "h2", "h3", "_filename"))
+    assert(dfResult2.drop("_filename").isEqual(df1))
+    assert(dfResult2.where($"_filename".isNull).isEmpty)
+  }
 
   private def createDataObject(path: String, schemaOpt: Option[StructType]): XmlFileDataObject = {
     val dataObj = XmlFileDataObject(id = "schemaTestXmlDO", path = path, schema = schemaOpt)
