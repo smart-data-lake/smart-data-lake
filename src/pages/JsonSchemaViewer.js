cddc012420275fef7d1d982fbd9a784c57263f75
Hiltpold Matthias <matthias.hiltpold@elca.ch>
2022-02-02 09:42:07 +0100

    fixed height issue of JsonSchemaViewer Component

--- src/pages/JsonSchemaViewer.js
+++ src/pages/JsonSchemaViewer.js
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from '@theme/Layout';
 
 function JsonSchemaViewer() {
-  const ref = React.useRef();
-  const [height, setHeight] = React.useState("0px");
+  
   const onLoad = () => {
-    setHeight(ref.current.contentWindow.document.body.scrollHeight + "px");
+    const wrapper = document.getElementsByClassName("main-wrapper")[0]
+    wrapper.style.display = 'flex'
   };
+  
   return (
-    <Layout title="JsonSchemaViewer">
-      <iframe
-        ref={ref}
-        onLoad={onLoad}
-        id="JsonSchemaViewer"
-        src="./json-schema-viewer/index.html"
-        width="100%"
-        height={height}
-        scrolling="no"
-        frameBorder="0"
-        style={{
-          //maxWidth: 640,
-          width: "100%",
-          overflow: "auto",
-        }}
-      ></iframe>
-    </Layout>
+    <Layout title="JsonSchemaViewer" >
+        <iframe
+          onLoad={onLoad}
+          id="JsonSchemaViewer"
+          src="./json-schema-viewer/index.html"
+          width="100%"
+          scrolling="no"
+          frameBorder="0"
+          style={{
+            flex: '1',
+          }}
+        ></iframe>
+    </Layout>    
   );
 }
 
