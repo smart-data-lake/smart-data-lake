2e1b5d0eab9c72ebc99da117b2f3d322515acd43
tbb <timo.babst@elca.ch>
2022-02-10 15:16:57 +0100

    first steps for websocket

---
+++ sdl-core/src/main/scala/io/smartdatalake/statusinfo/websocket/EventSocket.java
@@ -0,0 +1,66 @@
+//
+// ========================================================================
+// Copyright (c) Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package io.smartdatalake.statusinfo.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.eclipse.jetty.websocket.api.WebSocketAdapter;
+
+import java.util.Locale;
+import java.util.concurrent.CountDownLatch;
+
+public class EventSocket extends WebSocketAdapter
+{
+    private final CountDownLatch closureLatch = new CountDownLatch(1);
+
+    @Override
+    public void onWebSocketConnect(Session sess)
+    {
+        super.onWebSocketConnect(sess);
+        System.out.println("Socket Connected: " + sess);
+    }
+
+    @Override
+    public void onWebSocketText(String message)
+    {
+        super.onWebSocketText(message);
+        System.out.println("Received TEXT message: " + message);
+
+        if (message.toLowerCase(Locale.US).contains("bye"))
+        {
+            getSession().close(StatusCode.NORMAL, "Thanks");
+        }
+    }
+
+    @Override
+    public void onWebSocketClose(int statusCode, String reason)
+    {
+        super.onWebSocketClose(statusCode, reason);
+        System.out.println("Socket Closed: [" + statusCode + "] " + reason);
+        closureLatch.countDown();
+    }
+
+    @Override
+    public void onWebSocketError(Throwable cause)
+    {
+        super.onWebSocketError(cause);
+        cause.printStackTrace(System.err);
+    }
+
+    public void awaitClosure() throws InterruptedException
+    {
+        System.out.println("Awaiting closure from remote");
+        closureLatch.await();
+    }
+}
