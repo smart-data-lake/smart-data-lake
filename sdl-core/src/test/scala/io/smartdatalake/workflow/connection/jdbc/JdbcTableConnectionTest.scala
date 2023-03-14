b01a6fe95d85030fe095e9c930c10b79087172a7
Deuber David, PF85 <david.deuber.1@postfinance.ch>
2023-03-14 14:50:01 +0100

    Move JdbcTableConnectionTest into correct package (#633)

--- sdl-core/src/test/scala/io/smartdatalake/workflow/connection/jdbc/JdbcTableConnectionTest.scala
+++ sdl-core/src/test/scala/io/smartdatalake/workflow/connection/jdbc/JdbcTableConnectionTest.scala
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package io.smartdatalake.workflow.connection
+package io.smartdatalake.workflow.connection.jdbc
 
 import org.scalatest.FunSuite
 
