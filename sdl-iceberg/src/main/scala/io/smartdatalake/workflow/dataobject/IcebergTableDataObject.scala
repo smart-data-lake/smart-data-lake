d074d63a89b1d2d08ed2703fa5b820eb1481893a
Kull Zacharias (IT-SCF-F-BIM) <zacharias.kull@sbb.ch>
2023-03-31 18:10:54 +0200

    Let Iceberg create default catalog automatically
    This is to mimic the same behaviour as for the spark_catalog, where default database is always existing.

--- sdl-iceberg/src/main/scala/io/smartdatalake/workflow/dataobject/IcebergTableDataObject.scala
+++ sdl-iceberg/src/main/scala/io/smartdatalake/workflow/dataobject/IcebergTableDataObject.scala
@@ -170,7 +170,10 @@ case class IcebergTableDataObject(override val id: DataObjectId,
       require(session.conf.getOption("spark.sql.extensions").toSeq.flatMap(_.split(',')).contains("org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"),
         s"($id) Iceberg spark properties are missing. Please set spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions and org.apache.iceberg.spark.SparkSessionCatalog")
     }
-    require(isDbExisting, s"($id) DB ${table.db.get} doesn't exist (needs to be created manually).")
+    if(!isDbExisting) {
+      // DB (schema) is created automatically by iceberg when creatig tables. But we would like to keep the same behaviour as done by spark_catalog, where only default DB is existing, and others must be created manually.
+      require(table.db.contains("default"), s"($id) DB ${table.db.get} doesn't exist (needs to be created manually).")
+    }
     if (!isTableExisting) {
       require(path.isDefined, s"($id) If DeltaLake table does not exist yet, path must be set.")
       if (filesystem.exists(hadoopPath)) {
