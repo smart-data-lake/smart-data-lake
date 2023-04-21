b329445b6892a91e19b20dd1ff5c788a02396c35
Mandes Schönherr <mandes.schoenherr@id.unibe.ch>
2023-04-21 11:16:52 +0200

    add an entry point for Glue jobs filtering out all Glue specific arguments

---
+++ sdl-core/src/main/scala/io/smartdatalake/app/GlueSmartDataLakeBuilder.scala
@@ -0,0 +1,138 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2023 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.smartdatalake.app
+
+import io.smartdatalake.app.DatabricksSmartDataLakeBuilder.parser
+import io.smartdatalake.config.ConfigurationException
+import scopt.OParser
+
+/*
+ * AWS Glue Smart Data Lake Command Line Application.
+ *
+ * Glue passes all job parameters (command line arguments) down to the application. This application filters out the
+ *   Glue specific ones.
+ */
+class GlueSmartDataLakeBuilder extends SmartDataLakeBuilder {
+
+  def parseAndRun(args: Array[String], ignoreOverrideJars: Boolean = false): Unit = {
+    logger.info(s"Starting Program $appType $appVersion")
+
+    // filter the CLI arguments
+    def rmArg(sel: List[String], list: List[String]): List[String] = {
+      list match {
+        case Nil => sel
+        case "--additional-python-modules" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--class" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--continuous-log-conversionPattern" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--continuous-log-logGroup" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--continuous-log-logStreamPrefix" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--datalake-formats" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-auto-scaling" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-continuous-cloudwatch-log" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-continuous-log-filter" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-glue-datacatalog" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-job-insights" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-metrics" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-rename-algorithm-v2" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-s3-parquet-optimized-committer" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--enable-spark-ui" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--extra-files" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--extra-jars" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--extra-py-files" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--job-bookmark-option" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--job-language" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--python-modules-installer-option" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--scriptLocation" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--spark-event-logs-path" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--TempDir" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--use-postgres-driver" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--user-jars-first" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--conf" :: _ :: tail =>
+          rmArg(sel, tail)
+
+        case "--JOB_ID" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--JOB_RUN_ID" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "--JOB_NAME" :: _ :: tail =>
+          rmArg(sel, tail)
+        case "true" :: tail =>
+          rmArg(sel, tail)
+
+        case string :: tail =>
+          logger.debug("while filtering AWS GLUE options, found option for SDLB:" + string)
+          rmArg(sel :+ string, tail)
+      }
+    }
+
+    val options: Seq[String] = rmArg(List[String](), args.toList)
+    logger.info(s"Filtered command line args: +${options.mkString(";")}")
+
+    val scoptVersion = classOf[OParser[_, SmartDataLakeBuilderConfig]].getPackage.getImplementationVersion
+
+    OParser.parse(parser, options, SmartDataLakeBuilderConfig()) match {
+      case Some(config) =>
+        assert(config.overrideJars.isEmpty || ignoreOverrideJars, "Option override-jars is not supported by DefaultSmartDataLakeBuilder. Use DatabricksSmartDataLakeBuilder for this option.")
+        val stats = run(config)
+          .toSeq.sortBy(_._1).map(x => x._1 + "=" + x._2).mkString(" ") // convert stats to string
+        logger.info(s"$appType finished successfully: $stats")
+      case None =>
+        logAndThrowException(s"Aborting ${appType} after error", new ConfigurationException("Couldn't set command line parameters correctly."))
+    }
+  }
+}
+
+object GlueSmartDataLakeBuilder {
+
+  /**
+   * Entry-Point of the application.
+   *
+   * @param args Command-line arguments.
+   */
+  def main(args: Array[String]): Unit = {
+    val app = new GlueSmartDataLakeBuilder
+    app.parseAndRun(args)
+  }
+}
