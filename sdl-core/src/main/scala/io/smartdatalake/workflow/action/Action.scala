f421bfd21104627b29701107e19cdede08e4d9ba
pgruetter <patrick.gruetter@gmail.com>
2023-06-07 10:11:13 +0200

    678 - Remove reset in prepare phase to properly register prepare started event

--- sdl-core/src/main/scala/io/smartdatalake/workflow/action/Action.scala
+++ sdl-core/src/main/scala/io/smartdatalake/workflow/action/Action.scala
@@ -136,7 +136,6 @@ trait Action extends SdlConfigObject with ParsableFromConfig[Action] with DAGNod
    * This runs during the "prepare" phase of the DAG.
    */
   def prepare(implicit context: ActionPipelineContext): Unit = {
-    reset // reset statistics, this is especially needed in unit tests when the same action is started multiple times
     inputs.foreach(_.prepare)
     outputs.foreach(_.prepare)
     executionMode.foreach(_.prepare(id))
