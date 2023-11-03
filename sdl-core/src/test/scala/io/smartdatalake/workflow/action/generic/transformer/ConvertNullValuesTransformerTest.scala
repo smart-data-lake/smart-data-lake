0ef272ecbc733333611501821ebc126ac15cd8be
Thierry Girod <thierry.girod@elca.ch>
2023-11-03 10:59:23 +0100

    Add test for parameter exclusivity

---
+++ sdl-core/src/test/scala/io/smartdatalake/workflow/action/generic/transformer/ConvertNullValuesTransformerTest.scala
@@ -0,0 +1,53 @@
+/*
+ * Smart Data Lake - Build your data lake the smart way.
+ *
+ * Copyright © 2019-2023 ELCA Informatique SA (<https://www.elca.ch>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.smartdatalake.workflow.action.generic.transformer
+
+import io.smartdatalake.config.InstanceRegistry
+import io.smartdatalake.config.SdlConfigObject.DataObjectId
+import io.smartdatalake.testutils.TestUtil
+import io.smartdatalake.workflow.ActionPipelineContext
+import io.smartdatalake.workflow.dataframe.spark.SparkDataFrame
+import org.apache.spark.sql.SparkSession
+import org.scalatest.FunSuite
+
+class ConvertNullValuesTransformerTest extends FunSuite {
+
+  protected implicit val session: SparkSession = TestUtil.session
+  import session.implicits._
+
+  implicit val instanceRegistry = new InstanceRegistry()
+  implicit val context: ActionPipelineContext = TestUtil.getDefaultActionPipelineContext
+
+  test("exclusive white- or blacklisting check") {
+
+    // prepare
+    val convertNullValuesTransformer = ConvertNullValuesTransformer(columnWhitelist = Seq("column1"), columnBlacklist = Seq("column2"))
+    val df = SparkDataFrame(Seq((1, 1), (2, 2)).toDF("column1", "column2"))
+
+    // execute
+    val thrown = intercept[IllegalArgumentException]{
+      convertNullValuesTransformer.transform("id", Seq(), df, DataObjectId("dataObjectId"), None, Map())
+    }
+
+    // check
+    assert(thrown.isInstanceOf[IllegalArgumentException])
+    assert(thrown.getMessage == "requirement failed: Conflicting parameters. Please use either columnWhitelist or columnBlacklist, as simultaneous application is not supported.")
+  }
+}
