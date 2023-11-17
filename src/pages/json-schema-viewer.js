5ad741e3bab3b1a718afa023e62111eadaaefba3
zkull <zach.kull@gmail.com>
2023-11-17 20:43:53 +0100

    improve schema viewer height & overflow

--- src/pages/json-schema-viewer.js
+++ src/pages/json-schema-viewer.js
@@ -8,9 +8,7 @@ function JsonSchemaViewer() {
 
     return (
         <Layout title="JsonSchemaViewer" noFooter={true} wrapperClassName="schema-viewer-wrapper">
-            <div style={{height: "100vh"}}>
-                <SchemaViewerInBrowser  />
-            </div>
+            <SchemaViewerInBrowser />
         </Layout>
     );
 }
