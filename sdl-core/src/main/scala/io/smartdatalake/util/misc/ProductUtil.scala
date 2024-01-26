dab2103cb7a2bedaf5cd64fe744ba4768d37185e
Thierry Girod <thierry.girod@elca.ch>
2024-01-26 15:56:57 +0100

    Update (de) serializer

--- sdl-core/src/main/scala/io/smartdatalake/util/misc/ProductUtil.scala
+++ sdl-core/src/main/scala/io/smartdatalake/util/misc/ProductUtil.scala
@@ -20,7 +20,7 @@ package io.smartdatalake.util.misc
 
 import com.typesafe.config.Config
 import io.smartdatalake.config.SdlConfigObject.ConfigObjectId
-import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.catalyst.{DeserializerBuildHelper, ScalaReflection, SerializerBuildHelper}
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.{DataFrame, Dataset}
 
@@ -169,8 +169,8 @@ private[smartdatalake] object ProductUtil {
     val mirror = ScalaReflection.mirror
     val cls = mirror.runtimeClass(tpe)
     val encoder = ScalaReflection.encoderFor(tpe)
-    val serializer = ScalaReflection.serializerFor(encoder)
-    val deserializer = ScalaReflection.deserializerFor(encoder)
+    val serializer = SerializerBuildHelper.createSerializer(encoder)
+    val deserializer = DeserializerBuildHelper.createDeserializer(encoder)
     new ExpressionEncoder(serializer, deserializer, ClassTag(cls))
   }
 
