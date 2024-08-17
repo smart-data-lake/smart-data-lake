728bc441a49205cda77231de5e85ac49ff22f6df
Kull Zacharias <zacharias.kull@elca.ch>
2024-08-17 23:19:28 +0200

    refactor AzureADClientGrantAuthMode from CustomHttpAuthModeLogic to direct implementation of HttpAuthMode (#859)

--- sdl-azure/src/main/scala/io/smartdatalake/util/azure/AzureADClientGrantAuthMode.scala
+++ sdl-azure/src/main/scala/io/smartdatalake/util/azure/AzureADClientGrantAuthMode.scala
@@ -1,23 +1,16 @@
 package io.smartdatalake.util.azure
 
 import com.microsoft.aad.msal4j.{ClientCredentialFactory, ClientCredentialParameters, ConfidentialClientApplication}
+import com.typesafe.config.Config
+import io.smartdatalake.config.{FromConfigFactory, InstanceRegistry}
 import io.smartdatalake.util.misc.SmartDataLakeLogger
 import io.smartdatalake.util.secrets.StringOrSecret
-import io.smartdatalake.workflow.connection.authMode.CustomHttpAuthModeLogic
+import io.smartdatalake.workflow.connection.authMode.HttpAuthMode
 
 import java.util.Collections
 
-class AzureADClientGrantAuthMode extends CustomHttpAuthModeLogic with SmartDataLakeLogger {
-  var authority: StringOrSecret = _
-  var applicationId: StringOrSecret = _
-  var clientSecret: StringOrSecret = _
-  var scope: StringOrSecret = _
-  override def prepare(options: Map[String, StringOrSecret]): Unit = {
-    authority = options("authority")
-    applicationId = options("applicationId")
-    clientSecret = options("clientSecret")
-    scope = options("scope")
-  }
+case class AzureADClientGrantAuthMode(authority: StringOrSecret, applicationId: StringOrSecret, clientSecret: StringOrSecret, scope: StringOrSecret) extends HttpAuthMode with SmartDataLakeLogger {
+
   override def getHeaders: Map[String,String] = {
     logger.info(s"getting token from $authority")
     // building Azure AD client
@@ -32,5 +25,14 @@ class AzureADClientGrantAuthMode extends CustomHttpAuthModeLogic with SmartDataL
     logger.info(s"got token for $scope")
     // return as header
     Map("Authorization" -> s"Bearer $token")
+  }
+
+  override def factory: FromConfigFactory[HttpAuthMode] = AzureADClientGrantAuthMode
+
+}
+
+object AzureADClientGrantAuthMode extends FromConfigFactory[HttpAuthMode] {
+  override def fromConfig(config: Config)(implicit instanceRegistry: InstanceRegistry): AzureADClientGrantAuthMode = {
+    extract[AzureADClientGrantAuthMode](config)
   }
 }
