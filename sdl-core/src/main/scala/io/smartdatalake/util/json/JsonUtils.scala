5a95e6365612c872befb3c36ca3fb1a37a364a56
Kull Zacharias <zacharias.kull@elca.ch>
2024-11-12 23:44:45 +0100

    JsonUtils: implement converting to catalyst MapType

--- sdl-core/src/main/scala/io/smartdatalake/util/json/JsonUtils.scala
+++ sdl-core/src/main/scala/io/smartdatalake/util/json/JsonUtils.scala
@@ -5,9 +5,8 @@ import io.smartdatalake.util.misc.ProductUtil
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
 import org.apache.spark.sql.catalyst.{CatalystTypeConverters, InternalRow}
 import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.UTF8String
 import org.json4s.JsonAST._
-import org.json4s.jackson.{JsonMethods, Serialization}
+import org.json4s.jackson.{JsonMethods, Serialization, compactJson}
 import org.json4s.{Formats, JValue}
 
 import java.sql.Timestamp
@@ -38,6 +37,15 @@ object JsonUtils {
   }
 
   /**
+   * Convert a Json4s JObject into a Scala Map using a given MapType.
+   */
+  def convertObjectToMap(obj: JObject, dataType: MapType): Map[String, _] = {
+    obj.obj.map {
+      case (k, v) => (k, convertToCatalyst(v, dataType.valueType))
+    }.toMap
+  }
+
+  /**
    * Convert a Json4s JObject into InternalRows using a given Schema.
    * InternalRows can be used to create an RDD/DataFrame very efficiently.
    */
@@ -64,27 +72,30 @@ object JsonUtils {
 
   private def convertToCatalyst(value: Any, dataType: DataType): Any = {
     val scalaValue = (value, dataType) match {
-      case (x: Product, tpe: StructType) => convertProductToCatalyst(x, tpe)
       case (json: JObject, tpe: StructType) => convertObjectToCatalyst(json, tpe)
+      case (json: JObject, tpe: MapType) => convertObjectToMap(json, tpe)
       case (json: JArray, tpe: ArrayType) => json.arr.map(convertToCatalyst(_, tpe.elementType))
-      case (json: JString, tpe: StringType) => json.s
-      case (json: JLong, tpe: LongType) => json.num
-      case (json: JLong, tpe: IntegerType) => json.num.toInt
-      case (json: JInt, tpe: LongType) => json.num.toLong
-      case (json: JInt, tpe: IntegerType) => json.num.toInt
-      case (json: JInt, tpe: DecimalType) => BigDecimal(json.num)
-      case (json: JDecimal, tpe: DecimalType) => json.num
-      case (json: JDecimal, tpe: DoubleType) => json.num.toDouble
-      case (json: JDouble, tpe: DecimalType) => BigDecimal(json.num)
-      case (json: JDouble, tpe: DoubleType) => json.num
-      case (json: JDouble, tpe: FloatType) => json.num.toFloat
-      case (json: JBool, tpe: BooleanType) => json.value
-      case (json: JString, tpe: TimestampType) =>
+      case (json: JString, StringType) => json.s
+      case (json: JLong, LongType) => json.num
+      case (json: JLong, IntegerType) => json.num.toInt
+      case (json: JInt, LongType) => json.num.toLong
+      case (json: JInt, IntegerType) => json.num.toInt
+      case (json: JInt, _: DecimalType) => BigDecimal(json.num)
+      case (json: JDecimal, _: DecimalType) => json.num
+      case (json: JDecimal, DoubleType) => json.num.toDouble
+      case (json: JDouble, _: DecimalType) => BigDecimal(json.num)
+      case (json: JDouble, DoubleType) => json.num
+      case (json: JDouble, FloatType) => json.num.toFloat
+      case (json: JBool, BooleanType) => json.value
+      case (json: JString, TimestampType) =>
         Try(OffsetDateTime.parse(json.s).toInstant).toOption
           .getOrElse(Timestamp.valueOf(LocalDateTime.parse(json.s)))
-      case (json: JString, tpe: TimestampNTZType) => LocalDateTime.parse(json.s)
-      case (json: JString, tpe: DateType) => LocalDate.parse(json.s)
-      case (JNothing | JNull, _) => null
+      case (json: JString, TimestampNTZType) => LocalDateTime.parse(json.s)
+      case (json: JString, DateType) => LocalDate.parse(json.s)
+      case (x: Product, tpe: StructType) => convertProductToCatalyst(x, tpe)
+      case (x: JValue, StringType) => compactJson(x)
+      case (x: Any, StringType) => x.toString
+      case (JNothing | JNull | null, _) => null
     }
     CatalystTypeConverters.convertToCatalyst(scalaValue)
   }
