1bdd6a8dc34583ac4d64465f82bc391b2bbc94fb
Kull Zacharias <zacharias.kull@elca.ch>
2025-01-19 22:06:18 +0100

    fix typo in command line help

--- sdl-core/src/main/scala/io/smartdatalake/app/SmartDataLakeBuilder.scala
+++ sdl-core/src/main/scala/io/smartdatalake/app/SmartDataLakeBuilder.scala
@@ -18,7 +18,6 @@
  */
 package io.smartdatalake.app
 
-import com.typesafe.config.Config
 import io.smartdatalake.app
 import io.smartdatalake.communication.statusinfo.StatusInfoServer
 import io.smartdatalake.communication.statusinfo.api.SnapshotStatusInfoListener
@@ -300,7 +299,7 @@ abstract class SmartDataLakeBuilder extends SmartDataLakeLogger {
           val latestRunState = stateStore.recoverRunState(latestStateId)
           if (!latestRunState.isFinal || latestRunState.isFailed) {
             // start recovery
-            assert(appConfig == latestRunState.appConfig, s"There is a failed run to be recovered. Either you clean-up this state fail or the command line parameters given must match the parameters of the run to be recovered: ConfigToRecover=${latestRunState.appConfig} ConfigGiven=${appConfig}")
+            assert(appConfig == latestRunState.appConfig, s"There is a failed run to be recovered. Either you clean-up this state file or the command line parameters given must match the parameters of the run to be recovered: ConfigToRecover=${latestRunState.appConfig} ConfigGiven=${appConfig}")
             recoverRun(appConfig, stateStore, latestRunState)._2
           } else {
             val nextExecutionId = SDLExecutionId(latestRunState.runId + 1)
