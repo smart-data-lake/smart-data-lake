a8a02c095ebdf8f175feafebb9430436223ca130
thierry.girod <thierry.girod@hotmail.com>
2025-03-30 15:09:18 +0200

    Organize imports

--- sdl-debezium/src/main/scala/io/smartdatalake/workflow/connection/DebeziumConnection.scala
+++ sdl-debezium/src/main/scala/io/smartdatalake/workflow/connection/DebeziumConnection.scala
@@ -19,12 +19,11 @@
 
 package io.smartdatalake.workflow.connection
 import com.typesafe.config.Config
-import io.smartdatalake.config.SdlConfigObject.ConnectionId
-import io.smartdatalake.config.{FromConfigFactory, InstanceRegistry}
 import io.debezium.connector.mysql.MySqlConnector
 import io.debezium.connector.postgresql.PostgresConnector
+import io.smartdatalake.config.SdlConfigObject.ConnectionId
+import io.smartdatalake.config.{FromConfigFactory, InstanceRegistry}
 import io.smartdatalake.workflow.connection.authMode.{AuthMode, BasicAuthMode}
-import io.smartdatalake.workflow.connection.jdbc.JdbcTableConnection
 
 case class DebeziumConnection(override val id: ConnectionId,
                               uniqueConnectorName: String = java.util.UUID.randomUUID().toString,
