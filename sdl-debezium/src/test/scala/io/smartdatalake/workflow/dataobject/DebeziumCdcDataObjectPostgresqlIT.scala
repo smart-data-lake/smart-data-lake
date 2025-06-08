7f85fc011e09ad250ed96d34de6443e49d9a2378
Thierry Girod <thierry.girod@elca.ch>
2025-06-08 18:13:14 +0200

    Rework postgres integration test to reset after every test again.

--- sdl-debezium/src/test/scala/io/smartdatalake/workflow/dataobject/DebeziumCdcDataObjectPostgresqlIT.scala
+++ sdl-debezium/src/test/scala/io/smartdatalake/workflow/dataobject/DebeziumCdcDataObjectPostgresqlIT.scala
@@ -26,6 +26,7 @@ import io.smartdatalake.testutils.TestUtil
 import io.smartdatalake.util.hdfs.HdfsUtil
 import io.smartdatalake.util.misc.SmartDataLakeLogger
 import io.smartdatalake.util.secrets.StringOrSecret
+import io.smartdatalake.workflow.action.executionMode.DataObjectStateIncrementalMode
 import io.smartdatalake.workflow.action.{ActionMetadata, CopyAction}
 import io.smartdatalake.workflow.connection.DebeziumConnection
 import io.smartdatalake.workflow.connection.authMode.BasicAuthMode
@@ -46,25 +47,12 @@ object DebeziumCdcDataObjectPostgresqlIT extends App with SmartDataLakeLogger {
    * Init tests
    */
 
-  val sdlb = DefaultSmartDataLakeBuilder
-  implicit val instanceRegistry = sdlb.instanceRegistry
-  implicit val sparkSession = TestUtil.session
-  Environment._instanceRegistry = instanceRegistry
-  implicit val context = ConfigToolbox.getDefaultActionPipelineContext
 
+  implicit val sparkSession = TestUtil.session
   import sparkSession.implicits._
 
   val COMMIT_TYPE_COLUMN_NAME = "__commit_event"
   val COMMIT_TIMESTAMP_COLUMN_NAME = "__event_timestamp"
-
-  val connection = DebeziumConnection(
-    id = "dbzCon",
-    dbEngine = "postgresql",
-    hostname = sys.env("PSQL_HOSTNAME"),
-    db = Some(sys.env("PSQL_DB")),
-    port = sys.env("PSQL_PORT").toInt,
-    authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
-  )
 
   val jdbcConnection = JdbcTableConnection(
     id = "psqlCon",
@@ -81,108 +69,333 @@ object DebeziumCdcDataObjectPostgresqlIT extends App with SmartDataLakeLogger {
   implicit val filesystem: FileSystem = HdfsUtil.getHadoopFsWithDefaultConf(new Path(statePath))
   HdfsUtil.deleteFiles(new Path(statePath), doWarn = false)
 
-  instanceRegistry.register(connection)
-
-  jdbcConnection.execJdbcStatement("TRUNCATE demo.test")
-  jdbcConnection.execJdbcStatement("INSERT INTO demo.test (value, timestampCol, decimalCol) VALUES ('INIT 1', '1994-11-30 01:00:00', 19.94)")
-
-  // Setup data objects
-
-  val srcDO1 = DebeziumCdcDataObject("src1", connectionId = "dbzCon", Table(Some("demo"), "test"), debeziumProperties = Some(Map("database.server.id" -> "1234345345", "plugin.name" -> "pgoutput", "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory", "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat")))
-  instanceRegistry.register(srcDO1)
-
-  val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
-  instanceRegistry.register(tgtDO1)
-
-  // Setup copy actions
-
-  val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, metadata = Some(ActionMetadata(feed = Some(feedName))))
-  instanceRegistry.register(action1)
-
-  val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
-
-  // 1. Initial READ test
-
-  sdlb.run(sdlConfig)
-
-  var df = tgtDO1.getSparkDataFrame()
-
-  assert(df.columns.contains("id") &&
-    df.columns.contains("value") &&
-    df.columns.contains("timestampcol") &&
-    df.columns.contains("decimalcol") &&
-    df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
-    df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
-  )
-
-  assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("read")).filter(!$"test").isEmpty)
-
-  // 2. Insert test
-  jdbcConnection.execJdbcStatement("INSERT INTO demo.test (value, timestampCol, decimalCol) VALUES ('INSERT TEST', '1994-07-30 07:07:07', 30.0)")
-
-  sdlb.run(sdlConfig)
-
-  df = tgtDO1.getSparkDataFrame()
-
-  assert(df.columns.contains("id") &&
-    df.columns.contains("value") &&
-    df.columns.contains("timestampcol") &&
-    df.columns.contains("decimalcol") &&
-    df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
-    df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
-  )
-
-  assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("create")).filter(!$"test").isEmpty)
-
-  // 3. Update test
-  jdbcConnection.execJdbcStatement("UPDATE demo.test SET value = 'UPDATE TEST' WHERE value = 'INSERT TEST'")
-
-  sdlb.run(sdlConfig)
-
-  df = tgtDO1.getSparkDataFrame()
-
-  assert(df.columns.contains("id") &&
-    df.columns.contains("value") &&
-    df.columns.contains("timestampcol") &&
-    df.columns.contains("decimalcol") &&
-    df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
-    df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
-  )
-
-  assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME).isin(lit("update_preimage"), lit("update_postimage"))).collect().length == 2)
-
-  // 4. Delete test
-  jdbcConnection.execJdbcStatement("DELETE FROM demo.test WHERE value = 'UPDATE TEST'")
-
-  sdlb.run(sdlConfig)
-
-  df = tgtDO1.getSparkDataFrame()
-
-  assert(df.columns.contains("id") &&
-    df.columns.contains("value") &&
-    df.columns.contains("timestampcol") &&
-    df.columns.contains("decimalcol") &&
-    df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
-    df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
-  )
-
-  assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("delete")).filter(!$"test").isEmpty)
-
-  // 5. No new data test
-
-  sdlb.run(sdlConfig)
-
-  df = srcDO1.getSparkDataFrame() // check src because copyAction will be skipped and target will contain the data from previous test step
 
 
-  assert(df.columns.contains("id") &&
-    df.columns.contains("value") &&
-    df.columns.contains("timestampcol") &&
-    df.columns.contains("decimalcol") &&
-    df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
-    df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
-  )
 
-  assert(df.isEmpty)
+  def initialReadTest(): Unit = {
+
+    println("Initial read test started")
+
+    val sdlb = DefaultSmartDataLakeBuilder
+    implicit val instanceRegistry = sdlb.instanceRegistry
+    Environment._instanceRegistry = instanceRegistry
+    implicit val context = ConfigToolbox.getDefaultActionPipelineContext
+
+    // Setup connection
+    val connection = DebeziumConnection(
+      id = "dbzCon",
+      dbEngine = "postgresql",
+      hostname = sys.env("PSQL_HOSTNAME"),
+      db = Some(sys.env("PSQL_DB")),
+      port = sys.env("PSQL_PORT").toInt,
+      authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
+    )
+
+    instanceRegistry.register(connection)
+
+    // Setup data objects
+
+    val srcDO1 = DebeziumCdcDataObject("src1",
+      connectionId = "dbzCon",
+      Table(Some("demo"), "test"),
+      debeziumProperties = Some(Map(
+        "plugin.name" -> "pgoutput",
+        "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory",
+        "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat"
+      ))
+    )
+    instanceRegistry.register(srcDO1)
+
+    val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
+    instanceRegistry.register(tgtDO1)
+
+    // Setup copy actions
+
+    val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, executionMode = Some(DataObjectStateIncrementalMode()), metadata = Some(ActionMetadata(feed = Some(feedName))))
+    instanceRegistry.register(action1)
+
+    val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
+
+    // Do the initial inserts then run
+    jdbcConnection.execJdbcStatement("TRUNCATE demo.test")
+    jdbcConnection.execJdbcStatement("INSERT INTO demo.test (value, timestampCol, decimalCol) VALUES ('INIT 1', '1994-11-30 01:00:00', 19.94)")
+
+    sdlb.run(sdlConfig)
+
+    val df = tgtDO1.getSparkDataFrame()
+
+    assert(df.columns.contains("id") &&
+      df.columns.contains("value") &&
+      df.columns.contains("timestampcol") &&
+      df.columns.contains("decimalcol") &&
+      df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
+      df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
+    )
+
+    assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("read")).filter(!$"test").isEmpty)
+
+  }
+
+  initialReadTest()
+
+  def insertTest(): Unit = {
+
+    println("Insert test started")
+
+    val sdlb = DefaultSmartDataLakeBuilder
+    implicit val instanceRegistry = sdlb.instanceRegistry
+    Environment._instanceRegistry = instanceRegistry
+    implicit val context = ConfigToolbox.getDefaultActionPipelineContext
+
+    // Setup connection
+    val connection = DebeziumConnection(
+      id = "dbzCon",
+      dbEngine = "postgresql",
+      hostname = sys.env("PSQL_HOSTNAME"),
+      db = Some(sys.env("PSQL_DB")),
+      port = sys.env("PSQL_PORT").toInt,
+      authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
+    )
+
+    instanceRegistry.register(connection)
+
+    // Setup data objects
+
+    val srcDO1 = DebeziumCdcDataObject("src1",
+      connectionId = "dbzCon",
+      Table(Some("demo"), "test"),
+      debeziumProperties = Some(Map(
+        "plugin.name" -> "pgoutput",
+        "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory",
+        "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat"
+      ))
+    )
+    instanceRegistry.register(srcDO1)
+
+    val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
+    instanceRegistry.register(tgtDO1)
+
+    // Setup copy actions
+
+    val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, executionMode = Some(DataObjectStateIncrementalMode()), metadata = Some(ActionMetadata(feed = Some(feedName))))
+    instanceRegistry.register(action1)
+
+    val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
+
+    // Do the insert then run sdlb
+    jdbcConnection.execJdbcStatement("INSERT INTO demo.test (value, timestampCol, decimalCol) VALUES ('INSERT TEST', '1994-07-30 07:07:07', 30.0)")
+
+    sdlb.run(sdlConfig)
+
+    val df = tgtDO1.getSparkDataFrame()
+
+    assert(df.columns.contains("id") &&
+      df.columns.contains("value") &&
+      df.columns.contains("timestampcol") &&
+      df.columns.contains("decimalcol") &&
+      df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
+      df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
+    )
+
+    assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("create")).filter(!$"test").isEmpty)
+
+  }
+
+  insertTest()
+
+  def updateTest(): Unit = {
+
+    println("Update test started")
+
+    val sdlb = DefaultSmartDataLakeBuilder
+    implicit val instanceRegistry = sdlb.instanceRegistry
+    Environment._instanceRegistry = instanceRegistry
+    implicit val context = ConfigToolbox.getDefaultActionPipelineContext
+
+    // Setup connection
+    val connection = DebeziumConnection(
+      id = "dbzCon",
+      dbEngine = "postgresql",
+      hostname = sys.env("PSQL_HOSTNAME"),
+      db = Some(sys.env("PSQL_DB")),
+      port = sys.env("PSQL_PORT").toInt,
+      authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
+    )
+
+    instanceRegistry.register(connection)
+
+    // Setup data objects
+
+    val srcDO1 = DebeziumCdcDataObject("src1",
+      connectionId = "dbzCon",
+      Table(Some("demo"), "test"),
+      debeziumProperties = Some(Map(
+        "plugin.name" -> "pgoutput",
+        "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory",
+        "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat"
+      ))
+    )
+    instanceRegistry.register(srcDO1)
+
+    val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
+    instanceRegistry.register(tgtDO1)
+
+    // Setup copy actions
+
+    val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, executionMode = Some(DataObjectStateIncrementalMode()), metadata = Some(ActionMetadata(feed = Some(feedName))))
+    instanceRegistry.register(action1)
+
+    val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
+
+    // Do the update then run sdlb
+    jdbcConnection.execJdbcStatement("UPDATE demo.test SET value = 'UPDATE TEST' WHERE value = 'INSERT TEST'")
+
+    sdlb.run(sdlConfig)
+
+    val df = tgtDO1.getSparkDataFrame()
+
+    assert(df.columns.contains("id") &&
+      df.columns.contains("value") &&
+      df.columns.contains("timestampcol") &&
+      df.columns.contains("decimalcol") &&
+      df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
+      df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
+    )
+
+    assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME).isin(lit("update_preimage"), lit("update_postimage"))).collect().length == 2)
+
+  }
+
+  updateTest()
+
+  def deleteTest(): Unit = {
+
+    println("Delete test started")
+
+    val sdlb = DefaultSmartDataLakeBuilder
+    implicit val instanceRegistry = sdlb.instanceRegistry
+    Environment._instanceRegistry = instanceRegistry
+    implicit val context = ConfigToolbox.getDefaultActionPipelineContext
+
+    // Setup connection
+    val connection = DebeziumConnection(
+      id = "dbzCon",
+      dbEngine = "postgresql",
+      hostname = sys.env("PSQL_HOSTNAME"),
+      db = Some(sys.env("PSQL_DB")),
+      port = sys.env("PSQL_PORT").toInt,
+      authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
+    )
+
+    instanceRegistry.register(connection)
+
+    // Setup data objects
+
+    val srcDO1 = DebeziumCdcDataObject("src1",
+      connectionId = "dbzCon",
+      Table(Some("demo"), "test"),
+      debeziumProperties = Some(Map(
+        "plugin.name" -> "pgoutput",
+        "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory",
+        "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat"
+      ))
+    )
+    instanceRegistry.register(srcDO1)
+
+    val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
+    instanceRegistry.register(tgtDO1)
+
+    // Setup copy actions
+
+    val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, executionMode = Some(DataObjectStateIncrementalMode()), metadata = Some(ActionMetadata(feed = Some(feedName))))
+    instanceRegistry.register(action1)
+
+    val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
+
+    // Do the delete then run sdlb
+    jdbcConnection.execJdbcStatement("DELETE FROM demo.test WHERE value = 'UPDATE TEST'")
+
+    sdlb.run(sdlConfig)
+
+    val df = tgtDO1.getSparkDataFrame()
+
+    assert(df.columns.contains("id") &&
+      df.columns.contains("value") &&
+      df.columns.contains("timestampcol") &&
+      df.columns.contains("decimalcol") &&
+      df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
+      df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
+    )
+
+    assert(df.withColumn("test", col(COMMIT_TYPE_COLUMN_NAME) === lit("delete")).filter(!$"test").isEmpty)
+
+  }
+
+  deleteTest()
+
+  def noNewDataTest(): Unit = {
+
+    println("No new data test started")
+
+    val sdlb = DefaultSmartDataLakeBuilder
+    implicit val instanceRegistry = sdlb.instanceRegistry
+    Environment._instanceRegistry = instanceRegistry
+    implicit val context = ConfigToolbox.getDefaultActionPipelineContext
+
+    // Setup connection
+    val connection = DebeziumConnection(
+      id = "dbzCon",
+      dbEngine = "postgresql",
+      hostname = sys.env("PSQL_HOSTNAME"),
+      db = Some(sys.env("PSQL_DB")),
+      port = sys.env("PSQL_PORT").toInt,
+      authMode = BasicAuthMode(Some(StringOrSecret(sys.env("PSQL_USER"))), Some(StringOrSecret(sys.env("PSQL_PASSWORD"))))
+    )
+
+    instanceRegistry.register(connection)
+
+    // Setup data objects
+
+    val srcDO1 = DebeziumCdcDataObject("src1",
+      connectionId = "dbzCon",
+      Table(Some("demo"), "test"),
+      debeziumProperties = Some(Map(
+        "plugin.name" -> "pgoutput",
+        "schema.history.internal" -> "io.debezium.storage.file.history.FileSchemaHistory",
+        "schema.history.internal.file.filename" -> "C://TEMP/schemahistory.dat"
+      ))
+    )
+    instanceRegistry.register(srcDO1)
+
+    val tgtDO1 = ParquetFileDataObject("tgt1", tempDir.resolve("testTgt1").toString.replace('\\', '/'))
+    instanceRegistry.register(tgtDO1)
+
+    // Setup copy actions
+
+    val action1 = CopyAction("copyAction1", srcDO1.id, tgtDO1.id, executionMode = Some(DataObjectStateIncrementalMode()), metadata = Some(ActionMetadata(feed = Some(feedName))))
+    instanceRegistry.register(action1)
+
+    val sdlConfig = SmartDataLakeBuilderConfig(configuration = Seq("cp:/application.conf"), feedSel = feedName, applicationName = Some(appName), statePath = Some(statePath))
+
+    // Just run sdlb again
+
+    sdlb.run(sdlConfig)
+
+    val df = srcDO1.getSparkDataFrame() // check src because copyAction will be skipped and target will contain the data from previous test step
+
+
+    assert(df.columns.contains("id") &&
+      df.columns.contains("value") &&
+      df.columns.contains("timestampcol") &&
+      df.columns.contains("decimalcol") &&
+      df.columns.contains(COMMIT_TYPE_COLUMN_NAME) &&
+      df.columns.contains(COMMIT_TIMESTAMP_COLUMN_NAME)
+    )
+
+    assert(df.isEmpty)
+
+  }
+
+  noNewDataTest()
 
 }
