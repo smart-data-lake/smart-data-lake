55ac0243ead3ed1e078a4f9b582ed2b1e3b506fd
Kull Zacharias <zacharias.kull@elca.ch>
2025-10-17 21:45:17 +0200

    fix compile for scala 2.13

--- sdl-core/src/main/scala/io/smartdatalake/communication/agent/AgentServerController.scala
+++ sdl-core/src/main/scala/io/smartdatalake/communication/agent/AgentServerController.scala
@@ -48,19 +48,19 @@ case class AgentServerController(
 
             val receivedConfig = ConfigFactory.parseString(agentInstruction.hoconConfig, ConfigParseOptions.defaults().setSyntax(ConfigSyntax.CONF))
 
-            val localConnections = if (sdlbConfig.configuration.nonEmpty) {
+            val localConnections: Map[ConnectionId, Connection] = if (sdlbConfig.configuration.nonEmpty) {
               val localConfig = sdlbConfig.getHoconConfig(validateCompletness = false)
               getConnectionConfigMap(localConfig)
                 .map { case (id, config) => (ConnectionId(id), parseConfigObjectWithId[Connection](id, config)) }
             } else Map()
 
-            val connectionsToRegister = if (sdlbConfig.useOnlyLocalConnectionConfig) {
+            val connectionsToRegister: Map[ConnectionId, Connection] = if (sdlbConfig.useOnlyLocalConnectionConfig) {
               assert(sdlbConfig.configuration.nonEmpty, "No local configuration provided, set useOnlyLocalConnectionConfig=false or specify hocon configuration to use when starting the agent server.")
               localConnections
             } else {
               localConnections ++
-              getConnectionConfigMap(receivedConfig)
-                .map { case (id, config) => (ConnectionId(id), parseConfigObjectWithId[Connection](id, config)) }
+                getConnectionConfigMap(receivedConfig)
+                  .map { case (id, config) => (ConnectionId(id), parseConfigObjectWithId[Connection](id, config)) }
             }
             instanceRegistry.register(connectionsToRegister)
 
