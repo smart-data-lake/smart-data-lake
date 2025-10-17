6da0149be4fe80a92b028826336e92970126b5e4
Kull Zacharias <zacharias.kull@elca.ch>
2025-10-17 23:54:12 +0200

    allow multiple targets for ConfigJsonExporter and DataObjectSchemaExporter

--- sdl-lang/src/main/scala/io/smartdatalake/meta/configexporter/ConfigJsonExporter.scala
+++ sdl-lang/src/main/scala/io/smartdatalake/meta/configexporter/ConfigJsonExporter.scala
@@ -22,7 +22,7 @@ import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 import scala.util.Using
 
-case class ConfigJsonExporterConfig(configPaths: Seq[String] = null, target: String = "file:./exportedConfig.json", enrichOrigin: Boolean = true, descriptionPath: Option[String] = None, uploadDescriptions: Boolean = false)
+case class ConfigJsonExporterConfig(configPaths: Seq[String] = null, targets: Seq[String] = Seq("file:./exportedConfig.json"), enrichOrigin: Boolean = true, descriptionPath: Option[String] = None, uploadDescriptions: Boolean = false)
 
 object ConfigJsonExporter extends SmartDataLakeLogger {
 
@@ -35,10 +35,10 @@ object ConfigJsonExporter extends SmartDataLakeLogger {
       .action((value, c) => c.copy(configPaths = value.split(',')))
       .text("One or multiple configuration files or directories containing configuration files for SDLB, separated by comma.")
     opt[String]('f', "filename")
-      .action((value, c) => c.copy(target = "file:"+value))
+      .action((value, c) => c.copy(targets = Seq("file:" + value)))
       .text("Deprecated: Use target instead. File to export configuration to.")
     opt[String]('t', "target")
-      .action((value, c) => c.copy(target = value))
+      .action((value, c) => c.copy(targets = value.split(",").map(_.trim).toSeq))
       .text("Target URI to export configuration to. Can be 'file:./xyz.json', 'uiBackend', or any http/https URL. 'uiBackend will use global.uiBackend configuration to upload to UI backend. Default: file:./exportedConfig.json")
     opt[Boolean]("enrichOrigin")
       .action((value, c) => c.copy(enrichOrigin = value))
@@ -114,14 +114,18 @@ object ConfigJsonExporter extends SmartDataLakeLogger {
         val configAsJson = exportConfigJson(config)
 
         // create document writer depending on target uri scheme
-        val writer = ExportWriter.apply(config.target, config.configPaths)
+        val writers = config.targets.map(ExportWriter.apply(_, config.configPaths))
 
-        // write export config
-        val version = if (!config.target.contains("version=")) Some(getAppVersion()) else None
-        writer.writeConfig(configAsJson, version)
+        writers.zipWithIndex.foreach { case (writer, i) =>
 
-        // write descriptions
-        if (config.uploadDescriptions) uploadDescriptions(config, writer, version)
+          // write export config
+          val version = if (!config.targets(i).contains("version=")) Some(getAppVersion()) else None
+          writer.writeConfig(configAsJson, version)
+
+          // write descriptions
+          if (config.uploadDescriptions) uploadDescriptions(config, writer, version)
+        }
+
 
       case None =>
         logAndThrowException(s"Aborting ${appType} after error", new ConfigurationException("Couldn't set command line parameters correctly."))
