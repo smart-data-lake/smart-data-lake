1182d55ab9050e2fe15d8c9b4167fe7b6e186464
Kull Zacharias <zacharias.kull@elca.ch>
2025-11-30 13:07:06 +0100

    DataObjectSchemaExporterTest: fix tests

--- sdl-lang/src/test/scala/io/smartdatalake/meta/configexporter/DataObjectSchemaExporterTest.scala
+++ sdl-lang/src/test/scala/io/smartdatalake/meta/configexporter/DataObjectSchemaExporterTest.scala
@@ -130,7 +130,7 @@ class DataObjectSchemaExporterTest extends FunSuite with BeforeAndAfter {
   }
 
   test("test main with includes and excludes, dont update if same") {
-    DataObjectSchemaExporter.main(Array("-c", configPath, "-t", target, "-i", "dataObjectCsv[0-9]|dataObjectHive14", "-e", "dataObjectCsv5", "--preferredSubFeedType", "SparkSubFeed"))
+    DataObjectSchemaExporter.main(Array("-c", configPath, "-t", target, "-i", "dataObjectCsv[0-9]|dataObjectHive14", "-e", "dataObjectCsv5", "--preferredSubFeedType", "SparkSubFeed", "--stopOnError", "false"))
     assert(exportPath.toFile.listFiles().filter(_.getName.endsWith(".json")).map(_.getName.split('.').head).toSet == Set("dataObjectCsv1", "dataObjectCsv2", "dataObjectCsv3", "dataObjectCsv4", "dataObjectHive14"))
     assert(exportPath.toFile.listFiles().filter(_.getName.endsWith(".index")).map(_.getName.split('.').head).toSet == Set("dataObjectCsv1", "dataObjectCsv2", "dataObjectCsv3", "dataObjectCsv4", "dataObjectHive14"))
   }
