fc052fcf21fc2431e32c8df4b23c315baef0c75a
Nikolaus Thiel <klt@fsfe.org>
2026-06-25 09:15:27 +0200

    commit afc0127 from develop-spark3

--- sdl-core/src/main/scala/io/smartdatalake/config/ConfigParser.scala
+++ sdl-core/src/main/scala/io/smartdatalake/config/ConfigParser.scala
@@ -272,8 +272,25 @@ private[smartdatalake] object ConfigParser extends SmartDataLakeLogger {
   }
 
   /**
-   * Substitutes parts inside values by other paths of the configuration
-   * Token for substitution is "~{replacementPath}"
+   * Converts a value to snake_case: inserts an underscore at camelCase boundaries (including acronyms),
+   * replaces dashes with underscores and lowercases the result.
+   * Examples: "camelCase" -> "camel_case", "int-airports" -> "int_airports", "myHTTPServer" -> "my_http_server".
+   */
+  private[config] def toSnakeCase(value: String): String = {
+    value
+      .replaceAll("([a-z0-9])([A-Z])", "$1_$2") // camelCase boundary, e.g. camelCase -> camel_Case
+      .replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2") // acronym boundary, e.g. HTTPServer -> HTTP_Server
+      .replace('-', '_')
+      .toLowerCase
+  }
+
+  /**
+   * Substitutes parts inside values by other paths of the configuration.
+   * Token for substitution is "~{replacementPath}".
+   * An optional modifier can be appended with a pipe, e.g. "~{replacementPath|snake}", to transform the resolved value:
+   *  - "snake": converts the resolved value to snake_case, i.e. inserts underscores at camelCase boundaries, replaces
+   *    dashes ('-') with underscores ('_') and lowercases, e.g. to use an element id like 'int-airports' or 'intAirports'
+   *    as table name 'int_airports'.
    *
    * @param config configuration object for local substitution
    * @param path   path to search for local substitution tokens and execute substitution
@@ -283,16 +300,23 @@ private[smartdatalake] object ConfigParser extends SmartDataLakeLogger {
 
     val localSubstituter = (regMatch: Regex.Match) => {
       val replacementPath = regMatch.group(1)
-      if (config.hasPath(replacementPath)) {
-        if (config.getValue(replacementPath).valueType() == ConfigValueType.STRING
-          || config.getValue(replacementPath).valueType() == ConfigValueType.NUMBER) config.getString(replacementPath)
-        else throw ConfigurationException(s"local substitution path '$replacementPath' in path '$path' is not a string")
-      } else throw ConfigurationException(s"local substitution path '$replacementPath' in path '$path' does not exist")
+      val modifier = Option(regMatch.group(2))
+      val value =
+        if (config.hasPath(replacementPath)) {
+          if (config.getValue(replacementPath).valueType() == ConfigValueType.STRING
+            || config.getValue(replacementPath).valueType() == ConfigValueType.NUMBER) config.getString(replacementPath)
+          else throw ConfigurationException(s"local substitution path '$replacementPath' in path '$path' is not a string")
+        } else throw ConfigurationException(s"local substitution path '$replacementPath' in path '$path' does not exist")
+      modifier match {
+        case None => value
+        case Some("snake") => toSnakeCase(value)
+        case Some(m) => throw ConfigurationException(s"unknown local substitution modifier '$m' for path '$replacementPath' in path '$path'. Supported modifiers: snake")
+      }
     }
 
     if (config.hasPath(path) && config.getValue(path).valueType() == ConfigValueType.STRING) {
       val value = config.getString(path)
-      val valueSubstituted = """~\{(.*?)}""".r.replaceAllIn(value, localSubstituter)
+      val valueSubstituted = """~\{(.*?)(?:\|(\w+))?}""".r.replaceAllIn(value, localSubstituter)
       config.withValue(path, ConfigValueFactory.fromAnyRef(valueSubstituted))
     } else config
   }
--- sdl-core/src/test/scala/io/smartdatalake/config/ConfigParsingTest.scala
+++ sdl-core/src/test/scala/io/smartdatalake/config/ConfigParsingTest.scala
@@ -455,7 +455,6 @@ class ConfigParsingTest extends AnyFlatSpec with Matchers {
   }
 
   "TestDataObject" should "throw ConfigurationException on wrong connection type" in {
-    implicit val registry: InstanceRegistry = new InstanceRegistry()
     val config = ConfigFactory.parseString(
       """
         | connections {
@@ -554,7 +553,7 @@ class ConfigParsingTest extends AnyFlatSpec with Matchers {
         |  partitionColNb = 2
         | }
         | metadata {
-        |  test = test // doesnt exist
+        |  test = test // doesn't exist
         | }
         |}
         |
@@ -589,7 +588,7 @@ class ConfigParsingTest extends AnyFlatSpec with Matchers {
         | executionMode = {
         |  type = PartitionDiffMode
         |  partitionColNb = 2
-        |  test = test // doesnt exist
+        |  test = test // doesn't exist
         | }
         |}
         |
@@ -667,6 +666,79 @@ class ConfigParsingTest extends AnyFlatSpec with Matchers {
     refinedConfig.getString("pre-read-sql") shouldEqual "test10/abc"
     refinedConfig.getString("postWriteSql") shouldEqual "test10/abc"
     refinedConfig.getString("table.name") shouldEqual "test10/abc"
+  }
+
+  "local substitution with snake modifier" should "replace dashes with underscores" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = "int-airports"
+        | table {
+        |  name = "~{id|snake}"
+        | }
+        |}""".stripMargin
+    )
+
+    val configSubstituted = ConfigParser.localSubstitution(config, "table.name")
+    configSubstituted.getString("table.name") shouldEqual "int_airports"
+  }
+
+  it should "convert camelCase to snake_case" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = "intAirports"
+        | name = "~{id|snake}"
+        |}""".stripMargin
+    )
+
+    val configSubstituted = ConfigParser.localSubstitution(config, "name")
+    configSubstituted.getString("name") shouldEqual "int_airports"
+  }
+
+  it should "handle acronyms and mixed camelCase with dashes" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = "my-HTTPServerData"
+        | name = "~{id|snake}"
+        |}""".stripMargin
+    )
+
+    val configSubstituted = ConfigParser.localSubstitution(config, "name")
+    configSubstituted.getString("name") shouldEqual "my_http_server_data"
+  }
+
+  it should "leave a value without dashes unchanged" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = abc
+        | name = "~{id|snake}"
+        |}""".stripMargin
+    )
+
+    val configSubstituted = ConfigParser.localSubstitution(config, "name")
+    configSubstituted.getString("name") shouldEqual "abc"
+  }
+
+  it should "support mixing modified and plain tokens in the same value" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = "int-airports"
+        | name = "schema.~{id|snake}_~{id}"
+        |}""".stripMargin
+    )
+
+    val configSubstituted = ConfigParser.localSubstitution(config, "name")
+    configSubstituted.getString("name") shouldEqual "schema.int_airports_int-airports"
+  }
+
+  "local substitution with an unknown modifier" should "throw a ConfigurationException" in {
+    val config = ConfigFactory.parseString(
+      """{
+        | id = "int-airports"
+        | name = "~{id|kebab}"
+        |}""".stripMargin
+    )
+
+    intercept[ConfigurationException](ConfigParser.localSubstitution(config, "name"))
   }
 }
 
