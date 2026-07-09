14b7039f755ceff0a80cd9b737eb7aebf6541c7c
Nikolaus Thiel <Nikolaus.Thiel@elca.ch>
2026-07-09 15:07:13 +0200

    fixed ScalaNotebookSparkDfTransformerTest, working on LabCatalogGeneratorTest and DagExporterTest

--- sdl-lang/src/main/scala/io/smartdatalake/meta/dagexporter/DagExporter.scala
+++ sdl-lang/src/main/scala/io/smartdatalake/meta/dagexporter/DagExporter.scala
@@ -19,14 +19,16 @@
 package io.smartdatalake.meta.dagexporter
 
 import io.smartdatalake.config.{ConfigToolbox, ConfigurationException, InstanceRegistry}
-import io.smartdatalake.util.misc.SmartDataLakeLogger
+import io.smartdatalake.util.misc.{ProductUtil, SmartDataLakeLogger}
 import io.smartdatalake.workflow.action.Action
 import org.json4s.NoTypeHints
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.writePretty
 import scopt.OptionParser
 
-case class DagExporterConfig(sdlConfigPaths: String = null)
+case class DagExporterConfig(sdlConfigPaths: String = null) {
+  def toDebugString: String = ProductUtil.toDebugString(obj = this)
+}
 
 object DagExporter extends SmartDataLakeLogger {
 
@@ -45,7 +47,8 @@ object DagExporter extends SmartDataLakeLogger {
   }
 
   /**
-   * Takes as input a SDL Config and prints it's containing Actions to STDOUT, in a simplified JSON-Format.
+   * Takes as input a SDL Config and prints it's containing Actions to STDOUT, in a simplified
+   * JSON-Format.
    */
   def main(args: Array[String]): Unit = {
 
@@ -58,17 +61,18 @@ object DagExporter extends SmartDataLakeLogger {
         print(dagAsJSON)
         println("END DAG")
       case None =>
-        logAndThrowException(s"Aborting $appType after error", new ConfigurationException("Couldn't set command line parameters correctly."))
+        logAndThrowException(s"Aborting $appType after error",
+          new ConfigurationException("Couldn't set command line parameters correctly."))
     }
   }
 
-  private def toSimplifiedAction(action: Action): SimplifiedAction = {
+  private def toSimplifiedAction(action: Action): SimplifiedAction =
     SimplifiedAction(action.metadata, action.inputs.map(_.id.id), action.outputs.map(_.id.id))
-  }
 
   def exportConfigDagToJSON(config: DagExporterConfig): String = {
     val (registry, _) = ConfigToolbox.loadAndParseConfig(config.sdlConfigPaths.split(',').toIndexedSeq)
-    val simplifiedActions: Map[String, SimplifiedAction] = registry.getActions.groupBy(action => action.id.id).view.mapValues(action => toSimplifiedAction(action.head)).toMap
+    val simplifiedActions: Map[String, SimplifiedAction] =
+      registry.getActions.groupBy(action => action.id.id).view.mapValues(action => toSimplifiedAction(action.head)).toMap
     writePretty(simplifiedActions)(Serialization.formats(NoTypeHints))
   }
 }
